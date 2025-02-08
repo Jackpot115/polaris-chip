6a47ad56a9f4d9beb554efd8c56df086bcaa5eef
Matt C <mdc5904@psu.edu>
2025-02-08 18:22:09 -0500

    Homework Changes

--- src/my-card.js
+++ src/my-card.js
@@ -8,6 +8,13 @@ import { LitElement, html, css } from 'lit';
 
 export class MyCard extends LitElement {
 
+  static properties = {
+    title: { type: String },
+    image: { type: String },
+    linkText: { type: String },
+    backgroundColor: { type: String }
+  };
+
   static get tag() {
     return 'my-card';
   }
@@ -15,18 +22,57 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
+    this.image = 'https://psu-gatsby-files-prod.s3.amazonaws.com/s3fs-public/styles/16_9_1000w/public/2024/11/hax-2024.jpg?h=76593129&itok=Tiu2ETmI';
+    this.linkText = 'Click Me';
+    this.backgroundColor = '#42b6f5';
   }
 
   static get styles() {
     return css`
       :host {
         display: block;
+        justify-content: center;
+        align-items: center;
+        margin: 0;
       }
+      .card {
+      width: 300px;
+      border: 1px solid black;
+      padding: 16px;
+      text-align: center;
+      background-color: var(--card-bg, #42b6f5);
+    }
+      .card img {
+      width: 100%;
+      height: auto;
+    }
+      .card a {
+      display: none;
+      color: white;
+      background: blue;
+      padding: 8px 12px;
+      border-radius: 4px;
+      text-decoration: none;
+    }
+      .card h3 {
+      margin: 0 0 10px;
+    }
+      @media (max-width: 800px) and (min-width: 500px) {
+      .card a {
+        display: inline-block;
+      }
+    
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    <div class="card" style="background-color: ${this.backgroundColor}">
+        <h3>${this.title}</h3>
+        <img src="${this.image}" alt="${this.title}" />
+        <a href="#">${this.linkText}</a>
+      </div>
+    `;
   }
 
   static get properties() {
